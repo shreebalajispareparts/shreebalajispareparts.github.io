a56608c549069274872805674d19cdea5f9c1832
meMehtab <mehtabbir2@gmail.com>
2023-03-23 09:00:45 +0530

    changed endpoints to https

--- js_routing/cooling-coil.js
+++ js_routing/cooling-coil.js
@@ -1,4 +1,4 @@
-fetch('http://100.25.143.135:5000/visits/cooling-coil.html')
+fetch('https://100.25.143.135:5000/visits/cooling-coil.html')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error(error));
