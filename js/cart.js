c2fd2b08c6f70f67dcae1e3c13d2b75d3ea935cf
meMehtab <mehtabbir2@gmail.com>
2023-04-26 23:07:52 +0530

    Initiate Payment

--- js/cart.js
+++ js/cart.js
@@ -17,4 +17,96 @@ function addToCart(itemName, itemPrice){
     localStorage.setItem('cart', JSON.stringify(cart));
 }
 
- 
+ //Integrating Gpay API
+
+ function generate_paymentDataRequest(total_price){
+  const paymentDataRequest = {
+    merchantInfo: {
+      merchantId: 'merchant_id',
+      merchantName: 'merchant_name',
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPrice: `${total_price}`,
+      currencyCode: 'INR',
+    },
+    paymentMethodTokenizationParameters: {
+      tokenizationType: 'PAYMENT_GATEWAY',
+      parameters: {
+        gateway: 'your_gateway',
+        gatewayMerchantId: 'your_merchant_id',
+      },
+    },
+    allowedPaymentMethods: ['CARD', 'TOKENIZED_CARD'],
+    cardRequirements: {
+      allowedCardNetworks: ['VISA', 'MASTERCARD'],
+      billingAddressRequired: true,
+    },
+    shippingAddressRequired: true,
+    shippingAddressParameters: {
+      allowedCountryCodes: ['IN'],
+    },
+  };
+  console.log(paymentDataRequest);
+
+  const paymentsClient = new google.payments.api.PaymentsClient({
+    environment: 'TEST',
+    merchantInfo: {
+      merchantId: 'merchant_id',
+      merchantName: 'merchant_name',
+    },
+  });
+
+  paymentsClient.loadPaymentData(paymentDataRequest)
+  .then((paymentData) => {
+    
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let user = JSON.parse(sessionStorage.getItem('user')) || [];
+    // const createOrderRequest = {
+
+    // };
+     
+    let firstName = user.firstName;
+    let lastName = user.lastName;
+    let country = user.country;
+    let adress1 = user.adress1;
+    let address2 = user.address2;
+    let phone = user.phone;
+    let city = user.city;
+    let State = user.State;
+    let zipCode = user.zipCode;
+    let email = user.email;
+
+    let order_items = [];
+    cart.forEach(item => {
+        const order_item = {
+          "sku" : "",
+          "name" : item.name,
+          "quantity" : item.quantity,
+          "price" : item.price,
+          "weight" : "",
+          "dimensions":{
+            "length" : "",
+            "breadth" : "",
+            "height" : ""
+          }
+        } 
+        order_items.push(order_item);
+    });
+
+  console.log(order_items);
+    const endpointURL = `localhost:5000/create_order?orderId&order_date= "2019-07-24 11:11"&pickup_location="Kotkapura"&channel_id&comment&billing_customer_name=${firstName}&billing_last_name=${lastName}&billing_address=${adress1}&billing_address_2=${address2}&billing_city=${city}&billing_pincode=${zipCode}&billing_state=${State}&billing_country=${country}&billing_email=${email}&billing_phone=${phone}&shipping_is_billing="true"&order_items=${order_items}&payment_method="Prepaid"&shipping_charges=0&giftwrap_charges=0&transaction_charges=0&total_discount=0&sub_total`
+    fetch(endpointURL)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the Shiprocket API response
+    })
+    .catch(error => {
+      console.log("Api call error: " ,error);
+    });
+  })
+  .catch((error) => {
+    console.log("Google Api call error: " ,error);
+  });
+
+ }
