76823cb93feaa40ab65e2be6639a419290b3a2dd
meMehtab <mehtabbir2@gmail.com>
2023-06-13 00:45:17 +0530

    Implemented Google signin and Cart

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,21 @@
+// function addToCart(itemName, product_id, itemPrice){
+//     const productName = itemName;
+//     const productPrice = itemPrice;
+//     const product_id = product_id;
+//     const productQuantity = 1; // Default quantity is 1
 
-function addToCart(itemName, itemPrice){
-    const productName = itemName;
-    const productPrice = itemPrice;
-    const productQuantity = 1; // Default quantity is 1
+//     let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+//     let productIndex = cart.findIndex(item => item.name === productName);
 
-    let productIndex = cart.findIndex(item => item.name === productName);
-
-    if (productIndex !== -1) {
-      cart[productIndex].quantity += productQuantity;
-    } else {
-      cart.push({ name: productName, price: productPrice, quantity: productQuantity });
-    }
-    console.log(cart);
-    localStorage.setItem('cart', JSON.stringify(cart));
-}
+//     if (productIndex !== -1) {
+//       cart[productIndex].quantity += productQuantity;
+//     } else {
+//       cart.push({ name: productName, price: productPrice, quantity: productQuantity, id: product_id });
+//     }
+//     console.log(cart);
+//     localStorage.setItem('cart', JSON.stringify(cart));
+// }
 
  //Integrating Gpay API
 
